903f0254f40f61e217b5cd198a7ab53b44b8386c
Davis Sandefur <dsandefu@alumni.nd.edu>
2024-03-08 19:55:53 +0000

    Completed project

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,22 @@
 import numpy as np
 
-def calculate(list):
-
-
-
-
-    return calculations
+def calculate(x):
+    if len(x) < 9:
+        raise ValueError('List must contain nine numbers')
+    arr = np.array(x)
+    arr= arr.reshape(3,3)
+    mean = [np.mean(arr,axis=0).tolist(),np.mean(arr,axis=1).tolist(),np.mean(arr)]
+    var=[np.var(arr,axis=0).tolist(),np.var(arr,axis=1).tolist(),np.var(arr)]
+    sd=[np.std(arr,axis=0).tolist(),np.std(arr,axis=1).tolist(),np.std(arr)]
+    maxi=[np.max(arr,axis=0).tolist(),np.max(arr,axis=1).tolist(),np.max(arr)]
+    mini=[np.min(arr,axis=0).tolist(),np.min(arr,axis=1).tolist(),np.min(arr)]
+    summ=[np.sum(arr,axis=0).tolist(),np.sum(arr,axis=1).tolist(),np.sum(arr)]
+    calculations = {
+        'mean':mean,
+        'variance':var,
+        'standard deviation':sd,
+        'max':maxi,
+        'min':mini,
+        'sum':summ
+    }
+    return  calculations
